86c96b314f3bedae63a65b14b769ee5748720e57
Thomas Quan <tquan@fortyfournorth.ca>
2021-12-15 15:04:22 -0500

    add style attribute to iframe and img

--- packages/blog-utilities/src/module.test.ts
+++ packages/blog-utilities/src/module.test.ts
@@ -31,6 +31,67 @@ describe("Blog Utilities", () => {
             expect(generateHtmlFromJson(testJson)).toEqual(expectedHtml);
         });
 
+        test("Add custom style attribute to images", () => {
+            const dummy = {
+                type: "doc",
+                content: [
+                    {
+                        type: "image",
+                        attrs: {
+                            src: "https://res.cloudinary.com/fortyfournorth/image/upload/v1639537158/Test/ru3ccsrgirrp0san6tbi.jpg",
+                            alt: "Female head shot",
+                            title: null,
+                            style: "float:left padding-right:1rem"
+                        }
+                    },
+                    {
+                        type: "paragraph",
+                        attrs: {
+                            textAlign: "right"
+                        },
+                        content: [
+                            {
+                                type: "text",
+                                marks: [
+                                    {
+                                        type: "textStyle",
+                                        attrs: {
+                                            color: "rgba(220, 150, 150,1)"
+                                        }
+                                    }
+                                ],
+                                text: "What a wonderfull day it's"
+                            }
+                        ]
+                    }
+                ]
+            };
+            const expectedHTML = `<img src="https://res.cloudinary.com/fortyfournorth/image/upload/v1639537158/Test/ru3ccsrgirrp0san6tbi.jpg" alt="Female head shot" style="float:left padding-right:1rem"><p style="text-align: right"><span style="color: rgba(220, 150, 150,1)">What a wonderfull day it&apos;s</span></p>`;
+            expect(generateHtmlFromJson(dummy)).toEqual(expectedHTML);
+        });
+
+        test("Add custom style attribute to IFrame", () => {
+            const dummy = {
+                type: "doc",
+                content: [
+                    {
+                        type: "iframe",
+                        attrs: {
+                            src: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                            height: 150,
+                            width: 150,
+                            frameborder: 0,
+                            allowfullscreen: true,
+                            style: "float:right"
+                        }
+                    }
+                ]
+            };
+
+            const expectedHTML = `<div class="iframe-wrapper"><iframe src="https://www.youtube.com/watch?v=dQw4w9WgXcQ" height="150" width="150" frameborder="0" allowfullscreen style="float:right"></iframe></div>`;
+            expect(generateHtmlFromJson(dummy)).toEqual(expectedHTML);
+        });
+
         test("throws an error if no JSON is passed", () => {
             try {
                 //@ts-ignore - Not passing JSON
