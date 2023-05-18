ae6586c5f273c1a1f10c81edb4070433c3b9d84f
Thomas Quan <quantrithanh1999@gmail.com>
2023-05-18 13:55:21 -0400

    increase email domain name to 20

--- packages/utilities/src/module.test.ts
+++ packages/utilities/src/module.test.ts
@@ -301,44 +301,46 @@ describe("utilites", () => {
     describe("isValidEmailAddress", () => {
         // prettier-ignore
         test.each`
-            value                               | expected
-            ${"jt@mws-solutions.com"}                 | ${true}
-            ${"test@test.test"}                 | ${true}
-            ${"test@test.test "}                | ${false}
-            ${"test+test@test.test"}            | ${true}
-            ${"test@test.test.test"}            | ${true}
-            ${"email@example.com"}              | ${true}
-            ${"firstname.lastname@example.com"} | ${true}
-            ${"email@subdomain.example.com"}    | ${true}
-            ${"firstname+lastname@example.com"} | ${true}
-            ${"email@123.123.123.123"}          | ${true}
-            ${"email@[123.123.123.123]"}        | ${true}
-            ${'"email"@example.com'}            | ${false}
-            ${"1234567890@example.com"}         | ${true}
-            ${"email@example-one.com"}          | ${true}
-            ${"_______@example.com"}            | ${false}
-            ${"email@example.name"}             | ${true}
-            ${"email@example.museum"}           | ${true}
-            ${"email@example.co.jp"}            | ${true}
-            ${"firstname-lastname@example.com"} | ${true}
-            ${"test"}                           | ${false}
-            ${"testing@$$@.com"}                | ${false}
-            ${"plainaddress"}                   | ${false}
-            ${"#@%^%#$@#$@#.com"}               | ${false}
-            ${"@example.com"}                   | ${false}
-            ${"Joe Smith <email@example.com>"}  | ${false}
-            ${"email.example.com"}              | ${false}
-            ${"email@example@example.com"}      | ${false}
-            ${".email@example.com"}             | ${false}
-            ${"email.@example.com"}             | ${false}
-            ${"email..email@example.com"}       | ${false}
-            ${"email@example.com (Joe Smith)"}  | ${false}
-            ${"email@example"}                  | ${false}
-            ${"email@-example.com"}             | ${false}
-            ${"email@111.222.333.44444"}        | ${false}
-            ${"email@example..com"}             | ${false}
-            ${"Abc..123@example.com"}           | ${false}
-            ${"あいうえお@example.com"}         | ${false}
+            value                                    | expected
+            ${"jt@mws-solutions.com"}                | ${true}
+            ${"test@test.test"}                      | ${true}
+            ${"test@test.test "}                     | ${false}
+            ${"test+test@test.test"}                 | ${true}
+            ${"test@test.test.test"}                 | ${true}
+            ${"email@example.com"}                   | ${true}
+            ${"firstname.lastname@example.com"}      | ${true}
+            ${"email@subdomain.example.com"}         | ${true}
+            ${"firstname+lastname@example.com"}      | ${true}
+            ${"email@123.123.123.123"}               | ${true}
+            ${"email@[123.123.123.123]"}             | ${true}
+            ${"sebastine@michaelclark.construction"} | ${true}
+            ${"test+1@test.construction"}            | ${true}
+            ${'"email"@example.com'}                 | ${false}
+            ${"1234567890@example.com"}              | ${true}
+            ${"email@example-one.com"}               | ${true}
+            ${"_______@example.com"}                 | ${false}
+            ${"email@example.name"}                  | ${true}
+            ${"email@example.museum"}                | ${true}
+            ${"email@example.co.jp"}                 | ${true}
+            ${"firstname-lastname@example.com"}      | ${true}
+            ${"test"}                                | ${false}
+            ${"testing@$$@.com"}                     | ${false}
+            ${"plainaddress"}                        | ${false}
+            ${"#@%^%#$@#$@#.com"}                    | ${false}
+            ${"@example.com"}                        | ${false}
+            ${"Joe Smith <email@example.com>"}       | ${false}
+            ${"email.example.com"}                   | ${false}
+            ${"email@example@example.com"}           | ${false}
+            ${".email@example.com"}                  | ${false}
+            ${"email.@example.com"}                  | ${false}
+            ${"email..email@example.com"}            | ${false}
+            ${"email@example.com (Joe Smith)"}       | ${false}
+            ${"email@example"}                       | ${false}
+            ${"email@-example.com"}                  | ${false}
+            ${"email@111.222.333.44444"}             | ${false}
+            ${"email@example..com"}                  | ${false}
+            ${"Abc..123@example.com"}                | ${false}
+            ${"あいうえお@example.com"}              | ${false}
         `(`returns a "$expected" result for "$value"`, ({ value, expected }) => {
             expect(isValidEmailAddress(value)).toEqual(expected);
         });
